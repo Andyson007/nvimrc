2ae0a85df8e3d11a58c0032627d02736786e5ca9
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-12 07:20:44 +0200

    added long filename patch

--- init.lua
+++ init.lua
@@ -31,6 +31,9 @@ require("lazy").setup({
 			})
 		end,
 	},
+  {
+    "pixelastic/vim-undodir-tree"
+  }
 }, {})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
