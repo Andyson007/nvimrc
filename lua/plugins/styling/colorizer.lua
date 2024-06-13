b600fd6086329d31bc4b6ccff328dec9bdb3879d
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-13 08:16:48 +0200

    added toml coloring

--- lua/plugins/styling/colorizer.lua
+++ lua/plugins/styling/colorizer.lua
@@ -3,5 +3,6 @@ return {
 	opts = {
 		css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
 		html = { names = true }, -- Disable parsing "names" like Blue or Gray
+		toml = { names = true }, -- Disable parsing "names" like Blue or Gray
 	},
 }
