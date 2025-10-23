434c65cd5e6bbac04e664aedd8eb241106f4a868
MxAndy <>
2025-10-23 10:57:44 +0200

    update theme

--- lua/plugins/styling/theme.lua
+++ lua/plugins/styling/theme.lua
@@ -1,9 +1,9 @@
 return {
-  "Everblush/nvim",
-  name = "everblush",
+  "Shatur/neovim-ayu",
+  name = "ayu",
   priority = 1000,
   config = function()
-    require("everblush").setup({
+    require("ayu").setup({
       transparent = true,
       styles = {
         comments = { italic = false },
@@ -12,7 +12,7 @@ return {
         keywords = { italic = true },
       },
     })
-    vim.cmd("colorscheme everblush")
+    vim.cmd("colorscheme ayu")
     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = "lightgreen" })
     vim.api.nvim_set_hl(0, 'LineNr', { fg = "green" })
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = "lightred" })
