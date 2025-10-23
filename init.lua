a70d0b14759f9c869056c433c309e96c3281c2a1
MxAndy <andreas.jan.van.der.meulen@gmail.com>
2025-10-23 11:30:31 +0200

    removed useless comments

--- init.lua
+++ init.lua
@@ -23,8 +23,6 @@ require("lazy").setup({
     "pixelastic/vim-undodir-tree"
   }
 }, {})
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
 
 if vim.g.neovide then
   require("neovide")
